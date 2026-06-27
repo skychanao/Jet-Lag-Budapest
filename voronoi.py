82902b5b36ade5fb2511c2b553194681255bc6b6
skychanao <skyforunivesity@gmail.com>
2026-06-27 13:32:49 +0200

    changed line width

--- voronoi.py
+++ voronoi.py
@@ -245,7 +245,6 @@ def M_lines(m):
         name = "Metro Lines",
             style_function=lambda feature: {
                 'color': feature['properties'].get('colour', '#555555'),
-                'weight': 1
         },
         tooltip=folium.GeoJsonTooltip(
             fields = ['ref'] if 'ref' in metro_lines.columns else [],
@@ -261,7 +260,7 @@ def T_lines(m):
         name = "Tram Lines",
         style_function=lambda feature: {
             'color': '#e37c07',
-            'weight': 1
+            
         },
         show = True
     ).add_to(m)
